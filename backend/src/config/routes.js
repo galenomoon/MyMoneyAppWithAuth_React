bbca8e06f125a8043b0c75c77c02f8f56ec37669
galeno <gui.galenocover@gmail.com>
2022-01-28 15:46:03 -0300

    Authentication Backend - CORS + ROUTES + MIDDLEWARES

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const auth = require("./auth");
 
 module.exports = function (server) {
-  //Set base URL for each route
-  const router = express.Router();
-  server.use("/api", router);
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
+  protectedApi.use(auth);
 
-  //Route Payment Cycle
-  const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/AuthService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
 };
