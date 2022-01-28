a5c2e416e3794dbdec1291786e53f9a37872cf28
galeno <gui.galenocover@gmail.com>
2022-01-28 16:06:59 -0300

    AuthReducers + Navbar

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -1,8 +1,9 @@
 const userKey = "_mymoney_user";
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem(userKey)),
+  user: { name: "Teste", email: "teste@gmail.com" }, // JSON.parse(localStorage.getItem(userKey)),
   validToken: false,
 };
+
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "TOKEN_VALIDATED":
