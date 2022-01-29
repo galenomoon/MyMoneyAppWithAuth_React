498abbcbe1a8272113913a89409787b78514c8b6
galenomoon <gui.galenocover@gmail.com>
2022-01-29 00:59:06 -0300

    FIX IT | CORS Error

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,25 +1,24 @@
-const jwt = require("jsonwebtoken");
-const env = require("../.env");
-
+const jwt = require('jsonwebtoken')
+const env = require('../.env')
 module.exports = (req, res, next) => {
   // CORS preflight request
-  if (req.method === "OPTIONS") {
-    next();
+  if (req.method === 'OPTIONS') {
+    next()
   } else {
-    const token =
-      req.body.token || req.query.token || req.headers["authorization"];
+    const token = req.body.token || req.query.token ||
+      req.headers['authorization']
     if (!token) {
-      return res.status(403).send({ errors: ["No token provided."] });
+      return res.status(403).send({ errors: ['No token provided.'] })
     }
     jwt.verify(token, env.authSecret, function (err, decoded) {
       if (err) {
         return res.status(403).send({
-          errors: ["Failed to authenticate token."],
-        });
+          errors: ['Failed to authenticate token.']
+        })
       } else {
-        req.decoded = decoded;
-        next();
+        req.decoded = decoded
+        next()
       }
-    });
+    })
   }
-};
+}
